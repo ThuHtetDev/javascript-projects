0377b1930f4f97a28584539269161469297d0211
ThuhtetCoder <59603198+ThuhtetCoder@users.noreply.github.com>
2020-10-10 17:23:29 +0630

    pro01

--- 1-color-flipper/setup/app.js
+++ 1-color-flipper/setup/app.js
@@ -1,1 +1,11 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("changeBtn");
+const body = document.querySelector('body');
+btn.addEventListener('click',function(){ 
+        body.style.backgroundColor = colors[getSimpleRam()];
+        document.querySelector('.show').textContent = colors[getSimpleRam()];
+});
+
+function getSimpleRam(){
+    return Math.floor(Math.random() * Math.floor(colors.length))
+}
