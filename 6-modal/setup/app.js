f2c8be5f268879743baa201b3d12a73a6ea7e454
ThuhtetCoder <59603198+ThuhtetCoder@users.noreply.github.com>
2020-10-13 21:39:10 +0630

    6modal setup

--- 6-modal/setup/app.js
+++ 6-modal/setup/app.js
@@ -2,3 +2,16 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+
+const modelBtn = document.querySelector('.modelBtn');
+const model =document.querySelector(".model");
+const close = document.querySelector("#closeBtn");
+modelBtn.addEventListener('click',()=>{
+    model.classList.remove("nonblock");
+    model.classList.add("block");
+});
+
+close.addEventListener('click',()=>{
+    model.classList.add("nonblock");
+    model.classList.remove("block");
+});
